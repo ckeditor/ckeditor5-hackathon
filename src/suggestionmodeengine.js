55938ba8fd6f018549a435d6229ef4675527b78f
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-06-06 17:47:19 +0200

    Some basic refactoring.

--- src/suggestionmodeengine.js
+++ src/suggestionmodeengine.js
@@ -46,40 +46,7 @@ export default class SuggestionModeEngine extends Feature {
 				return;
 			}
 
-			const sel = data.selection;
-
-			// This is a bit tricky:
-			//
-			// 1. If deleting a non yet modified text, simply mark it.
-			// 2. If deleting a previously inserted text, then delete it as you would do normally.
-			//
-			// However, the selection may contain a bigger chunk of content with lots of inserted, deleted or whatever text.
-			// We shouldn't modify it node by node, but in bigger chunks. That's why we use filterNodes.
-			doc.enqueueChanges( () => {
-				const deletedRange = sel.getFirstRange();
-
-				sel.removeAllRanges();
-
-				// Case 1.
-				for ( let fragRange of filterNodes( deletedRange, isNotModified ) ) {
-					doc.batch().setAttr( DELETED, true, fragRange );
-				}
-
-				// Case 2.
-				let insertedRanges = Array.from( filterNodes( deletedRange, isInserted ) );
-				let fragRange;
-				let selectionPosition = deletedRange[ data.options.direction == 'FORWARD' ? 'end' : 'start' ];
-
-				while ( ( fragRange = insertedRanges.pop() ) ) {
-					// TODO Finding the expected selection position is more tricky than this.
-					// Case: "xx[xiix]xx" (i == inserted, x == existing). Now press backspace.
-					// Selection should end up at offset 2. It ends at offset 3.
-					selectionPosition = fragRange.start;
-					doc.batch().remove( fragRange );
-				}
-
-				sel.setRanges( [ new Range( selectionPosition, selectionPosition ) ] );
-			} );
+			this._handleDeletion( data.selection, data.options );
 
 			evt.stop();
 		}, null, 0 );
@@ -95,37 +62,81 @@ export default class SuggestionModeEngine extends Feature {
 				return;
 			}
 
-			const lastOperation = getLastOperation( batch );
-			const lastDelta = lastOperation.delta;
+			this._handleInsertion( data.range, batch );
+		} );
+	}
 
-			// Checking the last operation type may be redundant after we checked it above (TODO).
-			// However, checking the delta type let's us know whether it wasn't e.g. a split delta,
-			// which isn't insertion. After all, we don't want to mark second part of split paragraph as inserted.
-			if ( !( lastOperation instanceof InsertOperation ) || !( lastDelta instanceof InsertDelta ) ) {
-				return;
+	// This is a bit tricky:
+	//
+	// 1. If deleting a not yet modified text, simply mark it as deleted.
+	// 2. If deleting a previously inserted text, then delete it as you would do normally.
+	//
+	// However, the selection may contain a bigger chunk of content with lots of inserted, deleted or original text.
+	// We shouldn't modify it node by node, but in bigger chunks. That's why we use filterNodes and have some fun below.
+	_handleDeletion( sel, options ) {
+		const doc = this.editor.document;
+
+		doc.enqueueChanges( () => {
+			const deletedRange = sel.getFirstRange();
+
+			sel.removeAllRanges();
+
+			// Case 1.
+			for ( let fragRange of filterNodes( deletedRange, isNotModified ) ) {
+				doc.batch().setAttr( DELETED, true, fragRange );
 			}
 
-			// Do not mark text which was inserted inside a deleted text as inserted.
-			// This is how Google Docs work – typing within previously deleted text is still considered
-			// as removing this bit. Makes some sense.
-			if ( isInsertionWithinDeletion( data.range ) ) {
-				return;
+			// Case 2.
+			let insertedRanges = Array.from( filterNodes( deletedRange, isInserted ) );
+			let fragRange;
+			let selectionPosition = deletedRange[ options.direction == 'FORWARD' ? 'end' : 'start' ];
+
+			while ( ( fragRange = insertedRanges.pop() ) ) {
+				// TODO Finding the expected selection position is more tricky than this.
+				// Case: "xx[xiix]xx" (i == inserted, x == existing). Now press backspace.
+				// Selection should end up at offset 2. It ends at offset 3.
+				selectionPosition = fragRange.start;
+				doc.batch().remove( fragRange );
 			}
 
-			doc.enqueueChanges( () => {
-				// We're not checking type of the item (whether it's a text or element) so we'll mark elements
-				// as well. This is actually fine, but we'd need converters to render that. Also, it'd matter whether
-				// e.g. a right or left side of an block was merged with another block, so it's quite a lot of work
-				// which I'm ignoring at this point.
-				for ( let value of data.range ) {
-					const range = new Range( value.previousPosition, value.nextPosition );
+			sel.setRanges( [ new Range( selectionPosition, selectionPosition ) ] );
+		} );
+	}
 
-					batch.setAttr( INSERTED, true, range );
-					// We need to remove the deleted attribute, because typing uses weakinsert, so
-					// the text automatically inherits current attributes.
-					batch.removeAttr( DELETED, range );
-				}
-			} );
+	_handleInsertion( modifiedRange, batch ) {
+		const editor = this.editor;
+		const doc = editor.document;
+
+		const lastOperation = getLastOperation( batch );
+		const lastDelta = lastOperation.delta;
+
+		// Checking the last operation type may be redundant after we checked it above (TODO).
+		// However, checking the delta type let's us know whether it wasn't e.g. a split delta,
+		// which isn't insertion. After all, we don't want to mark second part of split paragraph as inserted.
+		if ( !( lastOperation instanceof InsertOperation ) || !( lastDelta instanceof InsertDelta ) ) {
+			return;
+		}
+
+		// Do not mark text which was inserted inside a deleted text as inserted.
+		// This is how Google Docs work – typing within previously deleted text is still considered
+		// as removing this bit. Makes some sense.
+		if ( isInsertionWithinDeletion( modifiedRange ) ) {
+			return;
+		}
+
+		doc.enqueueChanges( () => {
+			// We're not checking type of the item (whether it's a text or element) so we'll mark elements
+			// as well. This is actually fine, but we'd need converters to render that. Also, it'd matter whether
+			// e.g. a right or left side of an block was merged with another block, so it's quite a lot of work
+			// which I'm ignoring at this point.
+			for ( let value of modifiedRange ) {
+				const range = new Range( value.previousPosition, value.nextPosition );
+
+				batch.setAttr( INSERTED, true, range );
+				// We need to remove the deleted attribute, because typing uses weakinsert, so
+				// the text automatically inherits current attributes.
+				batch.removeAttr( DELETED, range );
+			}
 		} );
 	}
 }
