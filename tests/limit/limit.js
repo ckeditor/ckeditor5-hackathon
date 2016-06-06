d477dc4dbfbe9cd0e814f505a29c119d0105fcd4
Piotr Jasiun <p.jasiun@cksource.com>
2016-06-06 19:05:53 +0200

    Init.

---
+++ tests/limit/limit.js
@@ -0,0 +1,20 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* global console:false */
+
+'use strict';
+
+import ClassicEditor from '/ckeditor5/creator-classic/classic.js';
+
+ClassicEditor.create( document.querySelector( '#editor' ), {
+	features: [ 'delete', 'enter', 'typing', 'paragraph', 'hackathon/limit/limit' ]
+} )
+.then( newEditor => {
+	console.log( 'Editor was initialized', newEditor );
+} )
+.catch( err => {
+	console.error( err.stack );
+} );
