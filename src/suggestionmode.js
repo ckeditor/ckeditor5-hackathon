7c8f96f87e2bddb0c9e2ff3a76acbc621e572676
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-06-06 14:42:07 +0200

    Split suggestion mode feature into engine and UI parts.

--- src/suggestionmode.js
+++ src/suggestionmode.js
@@ -6,30 +6,22 @@
 'use strict';
 
 import Feature from '../feature.js';
-import Command from '../command/command.js';
-
-import BuildModelConverterFor from '../engine/conversion/model-converter-builder.js';
-
-import Range from '../engine/model/range.js';
-
-import InsertOperation from '../engine/model/operation/insertoperation.js';
 
 import ButtonController from '../ui/button/button.js';
 import ButtonView from '../ui/button/buttonview.js';
 import Model from '../ui/model.js';
 
-const INSERTED = 'inserted';
-const DELETED = 'deleted';
+import SuggestionModeEngine from './suggestionmodeengine.js';
 
 export default class SuggestionMode extends Feature {
+	static get requires() {
+		return [ SuggestionModeEngine ];
+	}
+
 	init() {
 		const editor = this.editor;
-		const editing = editor.editing;
-		const doc = editor.document;
 		const t = editor.t;
-		const command = new SuggestionModeCommand( editor );
-
-		editor.commands.set( 'suggestionMode', command );
+		const command = editor.commands.get( 'suggestionMode' );
 
 		// Create button model.
 		const buttonModel = new Model( {
@@ -47,77 +39,5 @@ export default class SuggestionMode extends Feature {
 
 		// Add suggestionMode button to feature components.
 		editor.ui.featureComponents.add( 'suggestionMode', ButtonController, ButtonView, buttonModel );
-
-		// Allow delete and insert attribute on all inline nodes.
-		doc.schema.allow( { name: '$inline', attributes: [ INSERTED ] } );
-		doc.schema.allow( { name: '$inline', attributes: [ DELETED ] } );
-
-		// Build converters from model to view for editing pipeline.
-		BuildModelConverterFor( editing.modelToView )
-			.fromAttribute( DELETED )
-			.toElement( 'del' );
-		BuildModelConverterFor( editing.modelToView )
-			.fromAttribute( INSERTED )
-			.toElement( 'ins' );
-
-		// Override default action of composer.deleteContents().
-		this.listenTo( doc.composer, 'deleteContents', ( evt, data ) => {
-			if ( !command.value ) {
-				return;
-			}
-
-			const sel = data.selection;
-
-			doc.enqueueChanges( () => {
-				doc.batch().setAttr( DELETED, true, sel.getFirstRange() );
-
-				const pos = sel.getFirstRange()[ data.options.direction == 'FORWARD' ? 'end' : 'start' ];
-				sel.setRanges( [ new Range( pos, pos ) ] );
-			} );
-
-			evt.stop();
-		}, null, 0 );
-
-		// Mark all inserted text.
-		this.listenTo( doc, 'change', ( evt, type, data, batch ) => {
-			if ( !command.value ) {
-				return;
-			}
-
-			if ( type != 'insert' ) {
-				return;
-			}
-
-			const lastOperation = getLastOperation( batch );
-
-			if ( !( lastOperation instanceof InsertOperation ) ) {
-				return;
-			}
-
-			for ( let value of data.range ) {
-				const range = new Range( value.previousPosition, value.nextPosition );
-				batch.setAttr( INSERTED, true, range );
-			}
-		} );
-	}
-}
-
-function getLastOperation( batch ) {
-	return last( last( batch.deltas ).operations );
-}
-
-function last( array ) {
-	return array[ array.length - 1 ];
-}
-
-class SuggestionModeCommand extends Command {
-	constructor( editor ) {
-		super( editor );
-
-		this.set( 'value', false );
-	}
-
-	_doExecute() {
-		this.value = !this.value;
 	}
 }
