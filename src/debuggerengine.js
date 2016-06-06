c3940de5790facfe7a738047b2d7ee7614001f61
Kamil Piechaczek <pomek@pomek.pl>
2016-06-06 16:11:58 +0200

    WIP: Added converted (it converts HTMLDocument to nested Maps

--- src/debuggerengine.js
+++ src/debuggerengine.js
@@ -6,7 +6,40 @@
 'use strict';
 
 import Feature from '../feature.js';
+// import DOMConventer from './domconventer';
 import { stringify } from '/tests/engine/_utils/model.js';
+
+function makeList( element ) {
+	const ul = document.createElement( 'ul' );
+	let li = document.createElement( 'li' );
+	let tagName;
+
+	ul.appendChild( li );
+
+	if ( element instanceof Text ) {
+		li.appendChild( element );
+	} else {
+		tagName = element.tagName.toLocaleLowerCase();
+
+		if ( tagName === 'body' ) {
+			tagName = '$root';
+		}
+
+		li.appendChild( new Text( `<${ tagName }>` ) );
+	}
+
+	for ( let item of element.childNodes ) {
+		li.appendChild( makeList( item ) );
+	}
+
+	if ( tagName ) {
+		li = document.createElement( 'li' );
+		li.appendChild( new Text( `</${ tagName }>` ) );
+		ul.appendChild( li );
+	}
+
+	return ul;
+}
 
 /**
  * @memberOf debugger
@@ -20,19 +53,26 @@ export default class DebuggerEngine extends Feature {
 	init() {
 		const editor = this.editor;
 		const doc = editor.document;
-
+		// const conventer = new DOMConventer();
 		const debugElement = window.document.createElement( 'div' );
-		debugElement.id = 'editor-debugger';
 		debugElement.style.border = '1px solid red';
-		debugElement.style.minHeight = '100px';
 
-		doc.on( 'change', ( ) => {
+		doc.on( 'change', () => {
 			doc.enqueueChanges( () => {
-				// const treeAsString = stringify( editor.document.getRoot().getChild( 0 ) );
-				// console.log( { treeAsString } );
+				const domParser = new DOMParser();
+				const modelAsString = stringify( doc.getRoot() );
+				const parsedDoc = domParser.parseFromString( modelAsString, 'text/html' );
+				// const convertedDoc = conventer.toArray( parsedDoc );
+
+				while ( debugElement.firstChild ) {
+					debugElement.removeChild( debugElement.firstChild );
+				}
+
+				debugElement.appendChild( makeList( parsedDoc.body ) );
 			} );
 		} );
 
+		// todo: refactor following lines:
 		window.stringify = stringify;
 		window.document.body.appendChild( debugElement );
 	}
---
+++ src/domconventer.js
@@ -0,0 +1,100 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+/**
+ * @memberOf debugger
+ */
+export default class DOMConventer {
+	/**
+	 * @param {HTMLDocument} document
+	 * @result {Array}
+	 */
+	toArray( document ) {
+		const parsedDocument = [];
+
+		for ( let item of document.body.childNodes ) {
+			const parsedElement = parseElement( item );
+			parsedDocument.push( parsedElement );
+		}
+
+		return parsedDocument;
+	}
+}
+
+/**
+ * @param {HTMLElement} element
+ * @returns {Map}
+ */
+function parseElement( element ) {
+	const elementMap = new Map();
+	const values = [];
+
+	elementMap.set( 'name', element.tagName.toLocaleLowerCase() );
+	elementMap.set( 'values', values );
+
+	const childNodes = Array.from( element.childNodes )
+		.filter( node => node instanceof Text );
+
+	for ( let child of childNodes ) {
+		// todo: is element a block?
+
+		for ( let nodeMap of parseText( child ) ) {
+			values.push( nodeMap );
+		}
+	}
+
+	return elementMap;
+}
+
+/**
+ * @param {Text} text
+ * @return {Map[]}
+ */
+function parseText( text ) {
+	const regexp = /(.*)<\$text ([^>]+)>(.*)/gi;
+	const result = regexp.exec( text.wholeText );
+
+	if ( result === null ) {
+		return [ buildTextMap( text.wholeText ) ];
+	}
+
+	const textMaps = [];
+
+	if ( !!result[ 1 ] ) {
+		textMaps.push( buildTextMap( result[ 1 ] ) );
+	}
+
+	textMaps.push(
+		buildTextMap( result[ 3 ], result[ 2 ] )
+	);
+
+	return textMaps;
+}
+
+/**
+ * @param {String} text
+ * @param {String} attributesAsString
+ */
+function buildTextMap( text, attributesAsString = '' ) {
+	const textMap = new Map();
+	const attributesMap = new Map();
+
+	textMap.set( 'name', '$text' );
+	textMap.set( 'values', text );
+	textMap.set( 'attributes', attributesMap );
+
+	const attrs = attributesAsString
+		.split( ' ' )
+		.filter( item => !!item.trim().length )
+		.map( item => item.split( '=' ) );
+
+	for ( const [attrName, attrValue] of attrs ) {
+		attributesMap.set( attrName, attrValue === 'true' ? true : attrValue );
+	}
+
+	return textMap;
+}
