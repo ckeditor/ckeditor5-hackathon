0d2ee2d42aa48e525b48e308876522cfd6ebb7f4
Kamil Piechaczek <pomek@pomek.pl>
2016-06-07 13:24:52 +0200

    Huge refactor: changed way to displaying tree.

--- src/debugger.js
+++ src/debugger.js
@@ -5,22 +5,18 @@
 
 'use strict';
 
-import { stringify } from '/tests/engine/_utils/model.js';
 import Feature from '../feature.js';
-import DOMConverter from './domconverter';
+import Range from '../engine/model/range';
+import Element from '../engine/model/element';
+import TextProxy from '../engine/model/textproxy';
 
 export default class Debugger extends Feature {
 	/**
 	 * @inheritDoc
 	 */
 	init() {
-		this.debugElement = window.document.createElement( 'div' );
-		this.debugElement.classList.add( 'ck-editor__code' );
-
+		this.debugElement = makeElement( 'div', 'ck-editor__code' );
 		window.document.body.appendChild( this.debugElement );
-
-		this.domParser = new DOMParser();
-		this.domConventer = new DOMConverter();
 
 		// Draw tree.
 		this.listenTo( this.editor.document, 'change', () => {
@@ -35,15 +31,86 @@ export default class Debugger extends Feature {
 	 */
 	drawTree() {
 		const editor = this.editor;
-		const modelAsString = stringify( editor.document.getRoot() );
-		const parsedDoc = this.domParser.parseFromString( modelAsString, 'text/html' );
-		const convertedDoc = this.domConventer.toArray( parsedDoc );
+		const documentRoot = editor.document.getRoot();
 
 		while ( this.debugElement.firstChild ) {
 			this.debugElement.removeChild( this.debugElement.firstChild );
 		}
 
-		this.debugElement.appendChild( makeList( convertedDoc ) );
+		const tree = Range.createFromElement( documentRoot ).getWalker();
+
+		const rootList = makeElement( 'ol' );
+		const openRoot = makeElement( 'li', 'ck-editor__code-line' );
+		const closeRoot = makeElement( 'li', 'ck-editor__code-line' );
+		const openSpanRoot = makeElement( 'span', 'ck-editor__element' );
+		const closeSpanRoot = makeElement( 'span', 'ck-editor__element' );
+
+		openSpanRoot.appendChild( new Text( '<$root>' ) );
+		closeSpanRoot.appendChild( new Text( '</$root>' ) );
+
+		openRoot.appendChild( openSpanRoot );
+		closeRoot.appendChild( closeSpanRoot );
+
+		rootList.appendChild( openRoot );
+		rootList.appendChild( closeRoot );
+
+		let indent = 1;
+
+		for ( let treeElement of tree ) {
+			const listElement = makeElement( 'li', 'ck-editor__code-line' );
+
+			if ( treeElement.type === 'ELEMENT_END' ) {
+				indent -= 1;
+			}
+
+			const itemValue = makeElement( 'span' );
+			let elementName;
+			let elementContent;
+
+			if ( treeElement.item instanceof Element ) {
+				itemValue.classList.add( 'ck-editor__element' );
+
+				elementName = treeElement.item.name;
+
+				if ( treeElement.type === 'ELEMENT_START' ) {
+					elementContent = '<' + elementName + '>';
+				} else {
+					elementContent = '</' + elementName + '>';
+				}
+			} else if ( treeElement.item instanceof TextProxy ) {
+				itemValue.classList.add( 'ck-editor__text' );
+
+				elementName = '$text';
+				elementContent = treeElement.item.text;
+
+				for ( let [attr, value] of treeElement.item.getAttributes() ) {
+					const attribueSpan = makeElement(
+						'span',
+						'ck-editor__text-badge',
+						`ck-editor__text-badge--${ attr }`
+					);
+
+					attribueSpan.appendChild( new Text( `${ attr }:${ value }` ) );
+					listElement.appendChild( attribueSpan );
+				}
+			} else {
+				elementName = 'UNKNOWN_ELEMENT';
+				elementContent = '';
+			}
+
+			itemValue.appendChild( new Text( elementContent ) );
+
+			listElement.insertBefore( itemValue, listElement.firstChild );
+			listElement.insertBefore( new Text( ' '.repeat( 4 * indent ) ), itemValue );
+
+			rootList.insertBefore( listElement, rootList.lastChild );
+
+			if ( treeElement.type === 'ELEMENT_START' ) {
+				indent += 1;
+			}
+		}
+
+		this.debugElement.appendChild( rootList );
 	}
 }
 
@@ -57,88 +124,4 @@ function makeElement( elementName, ...classList ) {
 	classList.forEach( className => element.classList.add( className ) );
 
 	return element;
-}
-
-/**
- * @param {Map[]} mapTree
- * @param {HTMLElement|null} rootElement
- * @returns {HTMLElement}
- * @todo refactor!
- */
-function makeList( mapTree, rootElement = null ) {
-	if ( !rootElement ) {
-		rootElement = makeElement( 'ul' );
-	}
-
-	for ( let item of mapTree.values() ) {
-		let openElementList = makeElement( 'li', 'ck-editor--clickable' );
-		let closeElementList = makeElement( 'li', 'ck-editor--non-clickable', 'js-ignore-folding' );
-		let helperElementList = makeElement( 'li', 'ck-editor__list-helper' );
-		const tagValueOpenElement = makeElement( 'span', 'ck-editor__code-element' );
-		const elementName = item.get( 'name' );
-
-		tagValueOpenElement.appendChild( new Text( elementName ) );
-		openElementList.appendChild( new Text( '<' ) );
-		openElementList.appendChild( tagValueOpenElement );
-		openElementList.appendChild( new Text( '>' ) );
-
-		const tagValueCloseElement = makeElement( 'span', 'ck-editor__code-element', 'ck-editor--non-clickable', 'js-ignore-folding' );
-		tagValueCloseElement.appendChild( new Text( elementName ) );
-
-		closeElementList.appendChild( new Text( '</' ) );
-		closeElementList.appendChild( tagValueCloseElement );
-		closeElementList.appendChild( new Text( '>' ) );
-
-		if ( elementName !== '$text' ) {
-			rootElement.appendChild( openElementList );
-			rootElement.appendChild( makeList( item.get( 'values' ), openElementList ) );
-			rootElement.appendChild( closeElementList );
-			rootElement.appendChild( helperElementList );
-		} else {
-			let rootElementList = rootElement.querySelector( 'ul' );
-
-			if ( !rootElementList ) {
-				rootElementList = makeElement( 'ul' );
-				rootElement.appendChild( rootElementList );
-			}
-
-			let textList = makeElement( 'ul', 'ck-editor--non-clickable', 'js-ignore-folding' );
-			let textElementList = makeElement( 'li', 'ck-editor--non-clickable', 'js-ignore-folding', 'ck-editor__code-text' );
-
-			for ( let [attrName, attrValue] of item.get( 'attributes' ).entries() ) {
-				openElementList.insertBefore( new Text( ' ' ), openElementList.lastChild );
-
-				const attrNameElement = makeElement( 'span', 'ck-editor__code-attribute-name' );
-				attrNameElement.appendChild( new Text( attrName ) );
-
-				const attrValueElement = makeElement( 'span', 'ck-editor__code-attribute-value' );
-				attrValueElement.appendChild( new Text( attrValue.toString() ) );
-
-				openElementList.insertBefore( new Text( ' ' ), openElementList.lastChild );
-				openElementList.insertBefore( attrNameElement, openElementList.lastChild );
-				openElementList.insertBefore( new Text( '=' ), openElementList.lastChild );
-				openElementList.insertBefore( attrValueElement, openElementList.lastChild );
-			}
-
-			rootElementList.appendChild( openElementList );
-			openElementList.appendChild( textList );
-			rootElementList.appendChild( closeElementList );
-			rootElementList.appendChild( helperElementList );
-
-			// todo: is `values` a block element?
-			textElementList.appendChild( new Text( item.get( 'values' ) ) );
-			textList.appendChild( textElementList );
-		}
-
-		openElementList.addEventListener( 'click', ( e ) => {
-			if ( e.srcElement.classList.contains( 'js-ignore-folding' ) ) {
-				return;
-			}
-
-			e.stopPropagation();
-			e.currentTarget.classList.toggle( 'ck-editor__code-element--is-close' );
-		} );
-	}
-
-	return rootElement;
 }
